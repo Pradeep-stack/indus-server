26e49d95d4314c9df98f6c2074224790ecbb9767
Pradeep-React-Node <dev.react@vibgyorweb.com>
2024-07-07 20:24:56 +0530

    bdash

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addProduct, getProduct, deleteProduct } from "../controllers/product.controller";
+import { addProduct, getProduct, deleteProduct } from "../controllers/product.controller.js";
 
 const router = Router()
 
