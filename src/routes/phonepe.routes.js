e64faf80f05bd67c139020c2dcefdfc69e99088c
Pradeep-React-Node <dev.react@vibgyorweb.com>
2025-01-11 20:33:45 +0530

    new code

--- src/routes/phonepe.routes.js
+++ src/routes/phonepe.routes.js
@@ -16,16 +16,16 @@ const router = express.Router();
 
 
 
-const MERCHANT_KEY="944a13ea-89fe-47e2-bb9c-34bce182cdf1"
-const MERCHANT_ID="M22CA7BWH4KS2"
-// const MERCHANT_KEY="96434309-7796-489d-8924-ab56988a6076"
-// const MERCHANT_ID="PGTESTPAYUAT86"
+// const MERCHANT_KEY="944a13ea-89fe-47e2-bb9c-34bce182cdf1"
+// const MERCHANT_ID="M22CA7BWH4KS2"
+const MERCHANT_KEY="96434309-7796-489d-8924-ab56988a6076"
+const MERCHANT_ID="PGTESTPAYUAT86"
 
-const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
-const MERCHANT_STATUS_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status"
+// const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+// const MERCHANT_STATUS_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status"
 
-// const MERCHANT_BASE_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
-// const MERCHANT_STATUS_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status"
+const MERCHANT_BASE_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+const MERCHANT_STATUS_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status"
 
 const redirectUrl="https://api.indusdigicart.com/payment/status"
 // const redirectUrl="https://api.indusdigicart.com/payment/status"
