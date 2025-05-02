a02cf1dd91664faf76ac263ae6181f40c505ec31
Pradeep-React-Node <dev.react@vibgyorweb.com>
2025-05-02 12:57:47 +0530

    puppert on server

--- src/controllers/expouser.controller.js
+++ src/controllers/expouser.controller.js
@@ -293,7 +293,10 @@ const registerExpoUser = asyncHandler(async (req, res) => {
       const html = htmlContent(createdUser);
 
       try {
-        const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteer.launch({
+          headless: "new", 
+          args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
         const page = await browser.newPage();
 
         await page.setContent(html, { waitUntil: "networkidle0" });
