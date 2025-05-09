8bfe18e6a46e415855e7adf500bfaaba4b2b4e8a
Pradeep-React-Node <dev.react@vibgyorweb.com>
2025-05-09 18:50:28 +0530

    all type done

--- src/controllers/expouser.controller.js
+++ src/controllers/expouser.controller.js
@@ -205,7 +205,7 @@ const importExpoUsers = asyncHandler(async (req, res) => {
 
         // Check for existing user
         const existingUser = await ExpoUser.findOne({ phone: user.phone });
-        if (existingUser && existingUser.userType !== "exhibitor") {
+        if (existingUser && existingUser.userType !== "exhibitor" || user.userType !== "exhibitor") {
           throw new Error("Phone number already exists");
         }
 
